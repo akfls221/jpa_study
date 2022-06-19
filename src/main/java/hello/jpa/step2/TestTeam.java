d16dd31405d42a5cc672b2b96e5a7c7e45ab249d
엄태권 <akfls221@gmail.com>
2022-06-20 00:25:25 +0900

    연관관계 매핑 - 양방향 연관관계, 연관관계 주인 설정

--- src/main/java/hello/jpa/step2/TestTeam.java
+++ src/main/java/hello/jpa/step2/TestTeam.java
@@ -4,10 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @NoArgsConstructor
@@ -20,6 +19,9 @@ public class TestTeam {
     private Long id;
 
     private String teamName;
+
+    @OneToMany(mappedBy = "team")
+    private List<TestMember> members = new ArrayList<>();
 
     public TestTeam(String teamName) {
         this.teamName = teamName;
