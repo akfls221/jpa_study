6f1743a4fb59f0c58080711d052a58143b3fbd55
엄태권 <akfls221@gmail.com>
2022-07-16 21:41:47 +0900

    Member Calss 작성(@ElementCollection, @CollectionTable) 사용

---
+++ src/main/java/hello/jpa/valuetype/listtypeembedded/Member.java
@@ -0,0 +1,40 @@
+package hello.jpa.valuetype.listtypeembedded;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Entity
+@NoArgsConstructor
+@Data
+public class Member {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "MEMBER_ID")
+    private Long id;
+
+    private String name;
+
+    @Embedded
+    private Address homeAddress;
+
+    @ElementCollection
+    @CollectionTable(name = "FAVORITE_FOODS",
+            joinColumns = @JoinColumn(name = "MEMBER_ID"))
+    @Column(name = "FOOD_NAME")
+    private Set<String> favoriteFoods = new HashSet<>();
+
+    @ElementCollection
+    @CollectionTable(name = "ADDRESS",
+            joinColumns = @JoinColumn(name = "MEMBER_ID"))
+    private List<Address> addressHistory = new ArrayList<>();
+
+
+
+}
