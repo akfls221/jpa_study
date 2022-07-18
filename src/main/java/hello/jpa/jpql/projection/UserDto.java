4a97c1a8b24c480321230108520084fbd8e5a1bf
엄태권 <akfls221@gmail.com>
2022-07-19 02:48:38 +0900

    new 로 받을 UserDto 생성

---
+++ src/main/java/hello/jpa/jpql/projection/UserDto.java
@@ -0,0 +1,14 @@
+package hello.jpa.jpql.projection;
+
+import lombok.ToString;
+
+@ToString
+public class UserDto {
+    private String name;
+    private int age;
+
+    public UserDto(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
