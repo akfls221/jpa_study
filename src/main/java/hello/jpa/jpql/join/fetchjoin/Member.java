e2d444fc2b2660aaed630bd9139af5b4a24d875b
엄태권 <akfls221@gmail.com>
2022-07-22 02:10:01 +0900

    feat : Entity 추가

---
+++ src/main/java/hello/jpa/jpql/join/fetchjoin/Member.java
@@ -0,0 +1,26 @@
+package hello.jpa.jpql.join.fetchjoin;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor
+@Data
+public class Member {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "TEAM_ID")
+    private Team team;
+
+    public Member(String name) {
+        this.name = name;
+    }
+}
