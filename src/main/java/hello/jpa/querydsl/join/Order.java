af69e0d5cb91aff41c67bc95ce3000ff805cda72
엄태권 <akfls221@gmail.com>
2022-07-31 04:33:09 +0900

    feat : Entity 생성(Member와 OrderItem을 가지고 있음

---
+++ src/main/java/hello/jpa/querydsl/join/Order.java
@@ -0,0 +1,32 @@
+package hello.jpa.querydsl.join;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@NoArgsConstructor
+@Data
+@Table(name = "ORDERS")
+public class Order {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "order_id")
+    private Long id;
+
+    @OneToMany
+    @JoinColumn(name = "order_id")
+    private List<OrderItem> orderItemList = new ArrayList<>();
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private OrderMember member;
+
+    public void addItem(OrderItem orderItem) {
+        this.getOrderItemList().add(orderItem);
+    }
+}
