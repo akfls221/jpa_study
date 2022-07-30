4143bf86d0c1fbbbc71de0180d07073d731f7425
엄태권 <akfls221@gmail.com>
2022-07-31 04:43:44 +0900

    feat : join on절 테스트

--- src/main/java/hello/jpa/querydsl/join/Querydsl_JoinBasic.java
+++ src/main/java/hello/jpa/querydsl/join/Querydsl_JoinBasic.java
@@ -11,12 +11,13 @@ import javax.persistence.Persistence;
 
 import java.util.List;
 
-import static hello.jpa.practice1.QOrder.*;
-import static hello.jpa.practice1.QMember.*;
-import static hello.jpa.practice1.QOrderItem.*;
+import static hello.jpa.querydsl.join.QOrder.*;
+import static hello.jpa.querydsl.join.QOrderMember.*;
+import static hello.jpa.querydsl.join.QOrderItem.*;
 
 /**
  * join(연관관계 있는 join)
+ * join(조인 대상, 별칭으로 사용할 쿼리 타입(QClass)
  */
 public class Querydsl_JoinBasic {
 
@@ -33,28 +34,46 @@ public class Querydsl_JoinBasic {
         orderItems.setItemName("상품1");
         em.persist(orderItems);
 
-        OrderMember orderMember = new OrderMember();
-        orderMember.setName("엄태권");
-        orderMember.setAge(29);
-        em.persist(orderMember);
+        OrderMember member = new OrderMember();
+        member.setName("엄태권");
+        member.setAge(29);
+        em.persist(member);
 
         Order orders = new Order();
         orders.addItem(orderItems);
-        orders.setMember(orderMember);
+        orders.setOrderMember(member);
         em.persist(orders);
 
         em.flush();
         em.clear();
 
+        /**
+         * 기본 on절
+         * order0_.member_id=ordermembe1_.member_id 자동으로 나감
+         */
         List<?> findList = queryFactory
                 .selectFrom(order)
-                .join(order.member, member)
+                .join(order.orderMember, orderMember)
                 .leftJoin(order.orderItemList, orderItem)
                 .fetch();
 
         for (Object o : findList) {
             logger.info("this is join : {}", o);
         }
+
+        /**
+         * on 절 포함한 join
+         */
+        List<Order> result = queryFactory
+                .selectFrom(order)
+                .join(order.orderMember, orderMember)
+                .leftJoin(order.orderItemList, orderItem)
+                .on(orderMember.name.eq("엄태권"))
+                .fetch();
+        for (Order orderResult : result) {
+            logger.info("this is join... on where orderMember name = 엄태권 : {}", orderResult);
+        }
+
         tx.commit();
     }
 }
